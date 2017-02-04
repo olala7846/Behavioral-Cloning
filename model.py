33026666e521259638ab26bf41d974197d519319
Hsin-Cheng Chao <olala7846@gmail.com>
2017-02-04 18:10:36 +0800

    Create simple convolution model and train on data
    1. the result is not so nice https://youtu.be/_IjGER-nh7Y
    2. may need data augmentation / dropout/ recovery

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
 """The script used to create and train the model."""
-from keras.models import Model, Sequential
-from keras.layers import Input, Dense
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers.convolutional import Convolution2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
 from scipy.misc import imread
-from sklearn.model_selection import ShuffleSplit
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 import csv
@@ -24,31 +28,54 @@ with open('./driving_log.csv', 'r') as f:
     image_urls = np.array(image_urls)
     angles = np.array(angles)
 
-splitter = ShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
-for train_index, test_index in splitter.split(image_urls):
-    print(train_index)
-    path_train, angle_train = image_urls[train_index], angles[train_index]
-    path_test, angle_test = image_urls[test_index], angles[test_index]
+# train test split
+urls_train, urls_test, angles_train, angles_test = train_test_split(
+    image_urls, angles, test_size=0.33)
+
+
+def normalize(X):
+    a = -0.5
+    b = 0.5
+    x_min = 0
+    x_max = 255
+    return a + (X - x_min) * (b - a) / (x_max - x_min)
 
 
 def generate_data(paths, angles, batch_size=128):
     """Generator that generates batch data from paths and angles"""
-    total_size = len(paths)
-    for offset in range(0, batch_size, total_size):
-        stop = offset + batch_size
-        batch_paths = paths[offset:stop]
-        _X_batch = [imread(path).astype(np.float32) for path in batch_paths]
-        _y_batch = angles[offset:stop]
-        yield _X_batch, _y_batch
+    while True:
+        total_size = len(paths)
+        for offset in range(0, batch_size, total_size):
+            stop = offset + batch_size
+            batch_paths = paths[offset:stop]
+            _X_batch = [imread(path).astype(np.float32) for path in batch_paths]
+            _X_batch = np.array(_X_batch)
+            _X_batch_normalized = normalize(_X_batch)
+            _y_batch = angles[offset:stop]
+            yield _X_batch_normalized, _y_batch
+
 
 # TODO(Olala): define model here
-for i in range(3):
-    gen = generate_data(path_train, angle_train, batch_size=5)
-    X_batch, y_batch = next(gen)
-    for X, y in zip(X_batch, y_batch):
-        print(X, y)
+model = Sequential()
+model.add(Convolution2D(
+    32, 3, 3, border_mode='valid',
+    subsample=(2, 2), input_shape=(160, 320, 3)))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(2, 2)))
+model.add(Convolution2D(128, 3, 3, border_mode='valid', subsample=(1, 2)))
+model.add(Flatten())
+model.add(Dense(300))
+model.add(Dense(150))
+model.add(Dense(32))
+model.add(Dense(1))
+model.compile('Adam', 'mse', metrics=['mse'])
 
-# TODO(Olala): train model here
+# Train model
+batch_size = 128
+nb_epochs = 100
+gen = generate_data(urls_train, angles_train, batch_size=batch_size)
+samples_per_epoch = int(len(urls_train)/batch_size)
+model.fit_generator(gen, samples_per_epoch, nb_epochs)
 
-# TODO(Olala): save model to model.h5
+# Save trained model
+model.save('my_model.h5')
 
