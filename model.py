85a18e7273eba62ee8cb3f5b1abf999b2dd39e66
Hsin-Cheng Chao <olala7846@gmail.com>
2017-02-05 23:52:58 +0800

    Evaluate on test set and fix generator
    1. generator was not implement correctly
    2. Add test generator to view how my model work

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from scipy.misc import imread
 from sklearn.model_selection import train_test_split
 
 import numpy as np
+import logging
 import csv
 
 
@@ -41,45 +42,39 @@ def normalize(X):
     return a + (X - x_min) * (b - a) / (x_max - x_min)
 
 
-def _gen_single_data(paths, angles):
-    """Generate single image angle pair"""
-    num_paths = paths.shape[0]
-    num_angles = angles.shape[0]
-    assert num_paths == num_angles
-
-    while True:
-        for i in range(num_paths):
-            img_data = imread(paths[i]).astype(np.float32)
-            angle = angles[i]
-            yield img_data, angle
-
-
 def generate_data(paths, angles, batch_size=128):
     """Generator that generates batch data from paths and angles"""
-    single_data_generator = _gen_single_data(paths, angles)
-
     while True:
-        _X_batch = []
-        _y_batch = []
-        for i in range(batch_size):
-            img_data, angle = next(single_data_generator)
-            # flip half of the images to avoid inbalance training data
-            if i % 2 == 1:
-                img_data = np.fliplr(img_data)
-                angle = -1.0 * angle
+        num_paths = paths.shape[0]
+        num_angles = angles.shape[0]
+        assert num_paths == num_angles
 
-            # normalize image data into [-0.5~0.5]
-            img_normalized = normalize(img_data)
-            _X_batch.append(img_normalized)
-            _y_batch.append(angle)
+        for offset in range(0, num_paths, batch_size):
+            stop = offset + batch_size
+            paths_batch = paths[offset:stop]
+            angles_batch = angles[offset:stop]
+            X_batch = []
+            y_batch = []
+            for i in range(paths_batch.shape[0]):
+                a_path = paths_batch[i]
+                a_angle = angles_batch[i]
+                img_data = imread(a_path).astype(np.float32)
 
-        # turns python list into numpy array
-        _X_batch = np.array(_X_batch)
-        _y_batch = np.array(_y_batch)
-        yield _X_batch, _y_batch
+                if i % 2 == 1:
+                    img_data = np.fliplr(img_data)
+                    a_angle = -0.1 * a_angle
+
+                img_data_normalized = normalize(img_data)
+                X_batch.append(img_data_normalized)
+                y_batch.append(a_angle)
+            X_batch = np.array(X_batch)
+            y_batch = np.array(y_batch)
+
+        yield X_batch, y_batch
 
 
 # Define and compile model
+logging.info('Creating model')
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, input_shape=(160, 320, 3)))
 model.add(MaxPooling2D((2, 2)))
@@ -109,11 +104,20 @@ model.add(Dense(1))
 model.compile('Adam', 'mse', metrics=['mse'])
 
 # Train model
+logging.info('Start training...')
 batch_size = 128
-nb_epochs = 50
-gen = generate_data(urls_train, angles_train, batch_size=batch_size)
-samples_per_epoch = int(len(urls_train)/batch_size)
-model.fit_generator(gen, samples_per_epoch, nb_epochs)
+nb_epochs = 5
+
+train_gen = generate_data(urls_train, angles_train, batch_size=batch_size)
+samples_per_epoch = urls_train.shape[0]
+test_gen = generate_data(urls_test, angles_test, batch_size=batch_size)
+test_size = urls_test.shape[0]
+
+model.fit_generator(
+    train_gen, samples_per_epoch, nb_epochs,
+    validation_data=test_gen,
+    nb_val_samples=test_size
+)
 
 # Save trained model
 model.save('my_model.h5')
